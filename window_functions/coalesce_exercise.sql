9c31022f9f4796b2c83d38b4c7bb767b45bc15f7
Jonathan Siu <jsiu1248@gmail.com>
2022-10-13 08:36:19 -0700

    adding names so that multiple columns can be created. also, there were some data type issues.

--- window_functions/coalesce_exercise.sql
+++ window_functions/coalesce_exercise.sql
@@ -11,18 +11,19 @@ WITH april_17_data AS (
 SELECT
   market_date,
   -- good example!
-  COALESCE(open_price, lag_open_price),
+  open_price,
+  COALESCE(open_price, lag_open_price) as open_lag,
   -- 2 different types?!?
-  COALESCE(market_date, open_price),
+  --COALESCE(market_date, open_price), -- this line won't work because of different types
   -- does order matter?
-  COALESCE(open_price, numeric_column),
-  COALESCE(numeric_column, open_price),
+  COALESCE(open_price, numeric_column) as open_numeric,
+  COALESCE(numeric_column, open_price) as numeric_open,
   -- using a default
-  COALESCE(open_price, null_column, 9001),
+  COALESCE(open_price, null_column, 9001::NUMERIC) as open_null_numeric ,
   -- only one null column input?
-  COALESCE(null_column),
+  COALESCE(null_column) as null_column,
   -- multiple null inputs
-  COALESCE(null, null),
+  COALESCE(null, null) as null_null, 
   -- blended?
-  COALESCE(null, null_column)
+  COALESCE(null, null_column) as null_null_column
 FROM april_17_data;
